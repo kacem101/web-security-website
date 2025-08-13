5649183ac3a332c57a474e28ba1ecbeb93c265e3
kassem101 <kassem101@yahoo.fr>
2025-08-13 15:42:20 +0100

    Update backend dependencies after removing Docker

--- backend/routes/labs.js
+++ backend/routes/labs.js
@@ -1,30 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../middleware/authenticate');
-const { startLab, stopLab , checkFlag } = require('../services/dockerService');
 const User = require('../models/User');
 const mongoose = require('mongoose');
-// Route to start a lab
-router.post('/start/:labId', authenticate, async (req, res) => {
-  try {
-    const { labId } = req.params;
-    const labInfo = await startLab(labId);
-    res.status(200).json(labInfo);
-  } catch (error) {
-    res.status(500).json({ message: 'Error starting lab.', error: error.message });
-  }
-});
-
-// Route to stop a lab
-router.post('/stop/:containerId', authenticate, async (req, res) => {
-  try {
-    const { containerId } = req.params;
-    await stopLab(containerId);
-    res.status(200).json({ message: 'Lab stopped successfully.' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error stopping lab.', error: error.message });
-  }
-});
 
 // Route to submit a flag for a lab
 router.post('/check-flag/:labId', authenticate, async (req, res) => {
